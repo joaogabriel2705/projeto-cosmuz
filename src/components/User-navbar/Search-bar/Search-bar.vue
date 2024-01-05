62656334e7b749699a9b9cd5d26ecd8ddcdf55a4
João Gabriel <jgquadros2005@gmail.com>
2024-01-05 16:20:23 -0300

    feat(components): Adding the components User-navbar and Search-bar

---
+++ src/components/User-navbar/Search-bar/Search-bar.vue
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<template>
+    <div class="searchbar">
+        <!-- <img src="../../../assets/svg/icons/search-icon.svg" class="searchbar__icon" alt="icone de uma lupa"> -->
+
+        <input type="text" class="searchbar__input" placeholder="Buscar produto...">
+    </div>
+</template>
+
+
+<style lang="scss">
+    @import '_Search-bar.scss';
+</style>
---
+++ src/components/User-navbar/User-navbar.vue
@@ -0,0 +1,37 @@
+<script setup>
+import SearchBar from './Search-bar/Search-bar.vue';
+</script>
+
+<template>
+    <div class="navbar">
+        <div>
+            <h2>Olá, Visitante!</h2>
+        </div>
+
+        <SearchBar />
+
+        <ul class="navbar__user-icons">
+            <li class="user-icons-icon">
+                <a href="#" class="icon__link">
+                    <img src="../../assets/svg/icons/save-outline-icon.svg" class="user-icon user-icon--save" alt="Icone de salvar">
+                </a>
+            </li>
+
+            <li class="user-icons-icon">
+                <a href="#" class="icon__link">
+                    <img src="../../assets/svg/icons/doorbell-icon.svg" class="user-icon user-icon--doorbell" alt="Icone de notificação">
+                </a>
+            </li>
+
+            <li class="user-icons-icon">
+                <button class="icon-button icon-button--menu-vertical">
+                    <img src="../../assets/svg/icons/menu-vertical-icon.svg" class="user-icon user-icon--menu-vertical" alt="Icone de menu vertical">
+                </button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style lang="scss">
+@import '_User-navbar.scss';
+</style>
