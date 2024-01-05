e159296d1b485d90aa12b0b18637565403dced9f
João Gabriel <jgquadros2005@gmail.com>
2024-01-05 16:45:51 -0300

    style(App): Adding the tag main for semantic

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup>
 import Header from './components/Header/Header.vue';
+import UserNavbar from './components/User-navbar/User-navbar.vue';
 </script>
 
 <template>
   <Header />
+  <main>
+    <UserNavbar />
+  </main>
 </template>
 
 <style scoped>
+  main {
+    margin: 4em 5%
+  }
 </style>
